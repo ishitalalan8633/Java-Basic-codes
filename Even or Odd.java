d42c6f69b99ffb6d6d0637f064ebf21a23ef5374
ishitalalan8633 <134314918+ishitalalan8633@users.noreply.github.com>
2024-09-27 18:58:19 +0530

    Create Even or Odd.java

---
+++ Even or Odd.java
@@ -0,0 +1,16 @@
+class Nos {
+    public static void main(String[] args) {
+        int n = 5;
+        if(n%2 == 0){
+            System.out.println("Number is even");
+        }
+        if(n%2 != 0){
+            System.out.println("Number is odd");
+        }
+    }
+}
+
+OUTPUT
+  Number is odd
+
+=== Code Execution Successful ===

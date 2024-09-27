ed4eeffcf72acdeff1701c1b9cec9d968dada32b
ishitalalan8633 <134314918+ishitalalan8633@users.noreply.github.com>
2024-09-27 19:12:49 +0530

    Create GreatestOfTwoNos.java

---
+++ GreatestOfTwoNos.java
@@ -0,0 +1,16 @@
+class Nos {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        if(a>b){
+            System.out.println("a is greatest");
+        }else{
+            System.out.println("b is greatest");
+        }
+    }
+}
+
+OUTPUT
+  b is greatest
+
+=== Code Execution Successful ===

1406f8529ccd50015dc4df35a414d9a81b971084
ishitalalan8633 <134314918+ishitalalan8633@users.noreply.github.com>
2024-09-27 19:10:37 +0530

    Create SumOfNosInRange.java

---
+++ SumOfNosInRange.java
@@ -0,0 +1,16 @@
+class Nos {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int sum = 0;
+        for(int i =a; i<=b ; i++){
+            sum = sum + i;
+            
+        }
+        System.out.println(sum);
+    }
+}
+OUTPUT
+  45
+
+=== Code Execution Successful ===

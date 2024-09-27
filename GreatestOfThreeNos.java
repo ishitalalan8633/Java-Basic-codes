550a73e8346a1fc7ad66f7a75e9c9523389630e4
ishitalalan8633 <134314918+ishitalalan8633@users.noreply.github.com>
2024-09-27 19:15:50 +0530

    Create GreatestOfThreeNos.java

---
+++ GreatestOfThreeNos.java
@@ -0,0 +1,21 @@
+class Nos {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int c = 15;
+        if(a>b && a>c){
+            System.out.println("a is greatest");
+        }
+        if(b>a && b>c){
+            System.out.println("b is greatest");
+        } else{
+            System.out.println("c is greatest");
+        }
+        
+    }
+}
+
+OUTPUT
+  c is greatest
+
+=== Code Execution Successful ===

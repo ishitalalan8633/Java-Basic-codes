921296a9962db3d54e965cc5693a779832b0d134
ishitalalan8633 <134314918+ishitalalan8633@users.noreply.github.com>
2024-09-27 19:04:35 +0530

    Create SumOfNNaturalNos.java

---
+++ SumOfNNaturalNos.java
@@ -0,0 +1,12 @@
+class Nos {
+    public static void main(String[] args) {
+        int n = 5;
+        
+        System.out.println(n*(n+1)/2);
+    }
+}
+
+OUTPUT
+  15
+
+=== Code Execution Successful ===

d7c06ba3ea4c958bce542d039871d1212304d1e2
ishitalalan8633 <134314918+ishitalalan8633@users.noreply.github.com>
2024-09-27 19:24:51 +0530

    Create LeapYear.java

---
+++ LeapYear.java
@@ -0,0 +1,16 @@
+class LeapYear {
+    public static void main(String[] args) {
+        int a = 2016;
+        if(a%4==0 && a%100!=0){
+            System.out.println(" year is a leap year");
+        } else{
+            System.out.println("year is not a leap year");
+        }
+        
+    }
+}
+
+OUTPUT
+   year is a leap year
+
+=== Code Execution Successful ===

44cbfd2ecc9a1c4064fb7762b984e5a63f6fef35
ishitalalan8633 <134314918+ishitalalan8633@users.noreply.github.com>
2024-09-27 18:55:37 +0530

    Create Positive or negative nos.java

---
+++ Positive or negative nos.java
@@ -0,0 +1,19 @@
+class Nos {
+    public static void main(String[] args) {
+        int n = 5;
+        if(n>0){
+            System.out.println("Number is positive");
+        }
+        if(n<0){
+            System.out.println("Number is negative");
+        }
+        if(n == 0){
+            System.out.println("Number is zero");
+        }
+    }
+}
+
+OUTPUT
+  Number is positive
+
+=== Code Execution Successful ===
